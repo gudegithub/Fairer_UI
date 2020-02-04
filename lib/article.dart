9c1e49e7713d9aee6f0be2c08af11cf62554ba5f
tomotomy <tomo1999724@gmail.com>
2020-02-04 17:07:16 +0900

    separate widget

---
+++ lib/article.dart
@@ -0,0 +1,61 @@
+
+
+import 'package:flutter/material.dart';
+
+class ArticlePage extends StatelessWidget {
+  const ArticlePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: Colors.black38,
+          onPressed: () {
+            Navigator.of(context).pop(true);
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            color: Colors.black38,
+            onPressed: () {},
+          )
+        ],
+      ),
+      bottomNavigationBar: Container(
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.access_time),
+              title: Text("時間割"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              title: Text("カレンダー"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.description),
+              title: Text("メディア")
+            )
+          ],
+        ),
+      ),
+      endDrawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              child: ListTile(
+                
+              ),
+            ),
+            ListTile(),
+          ],
+        )
+      ),
+    );
+  }
+}
--- lib/media_index.dart
+++ lib/media_index.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'article.dart';
 
 class MediaIndexPage extends StatelessWidget {
   @override
@@ -48,16 +48,39 @@ class MediaIndexPage extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
-          Container(
-            padding: EdgeInsets.only(top: 10.0, left: 30.0),
-            child: Text(
-              "オススメのトピック",
-              style: TextStyle(
-                fontSize: 20.0,
-              )
-            )
-          ),
-          Container(
+          MediaIndexMessage(),
+          Category(),
+          ArticleList(),
+        ],
+      )
+    );
+  }
+}
+
+class MediaIndexMessage extends StatelessWidget {
+  const MediaIndexMessage ({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0, left: 30.0),
+      child: Text(
+        "オススメのトピック",
+        style: TextStyle(
+          fontSize: 20.0,
+        )
+      )
+    );
+  }
+}
+
+class Category extends StatelessWidget {
+  const Category({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
             padding: EdgeInsets.only(top: 20, bottom: 20),
             height: 150,
             child: ListView(
@@ -95,68 +118,106 @@ class MediaIndexPage extends StatelessWidget {
               ],
             )
           ),
-          Container(
-            padding: EdgeInsets.only(left: 30.0),
-            height: 400,
-            child: ListView(
-              children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(top: 30.0),
-                child: Row(
-                  children: <Widget>[
-                    SizedBox(
-                    width: 110,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.asset(
-                        'assets/image/thumbnail.png',
-                        fit: BoxFit.cover,
-                        )
-                      ),
-                    ),
-                    Flexible(
-                      child: Column(
-                        children: <Widget>[
-                          Container(
-                            child: Row(
-                              children: <Widget>[
-                                Container(
-                                  padding: EdgeInsets.only(left: 30.0, right: 30.0),
-                                  child: Image.asset(
-                                    'assets/image/Column_budge.png',
-                                    width: 75,
-                                  )
-                                ),
-                                Text(
-                                  "2019 8/20",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold
-                                  )
-                                )
-                              ],
-                            ),
-                          ),
-                          GestureDetector(
-                            child: Container(
-                              padding: EdgeInsets.only(top: 10.0,left: 30.0, right: 30.0),
-                              child: Text(
-                                '【2020年度最新版】日本でeスポーツ認定されているゲーム一覧',
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 2,
-                              )
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
+    );
+  }
+}
+
+class ArticleList extends StatelessWidget {
+  const ArticleList({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        padding: EdgeInsets.only(left: 30.0),
+        child: SingleChildScrollView(
+          child: Column(children: <Widget>[
+            Article(),
+            Article(),
+            Article(),
+            Article(),
+            Article(),
+            Article(),
+            Article(),
+          ],)
+        ),
+      ),
+    );
+  }
+}
+
+class Article extends StatelessWidget {
+  const Article({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Container(
+        margin: EdgeInsets.only(top: 30.0),
+        child: Row(
+          children: <Widget>[
+            GestureDetector(
+              child: SizedBox(
+              width: 110,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset(
+                  'assets/image/thumbnail.png',
+                  fit: BoxFit.cover,
+                  )
                 ),
               ),
-              
-            ],),
-          )
-        ],
-      )
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ArticlePage()),
+                );
+              },
+            ),
+            Flexible(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.only(left: 30.0, right: 30.0),
+                          child: Image.asset(
+                            'assets/image/Column_budge.png',
+                            width: 75,
+                          )
+                        ),
+                        Text(
+                          "2019 8/20",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold
+                          )
+                        )
+                      ],
+                    ),
+                  ),
+                  GestureDetector(
+                    child: Container(
+                      padding: EdgeInsets.only(top: 10.0,left: 30.0, right: 30.0),
+                      child: Text(
+                        '【2020年度最新版】日本でeスポーツ認定されているゲーム一覧',
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 2,
+                      )
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ArticlePage()),
+                      );
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
